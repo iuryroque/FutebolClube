86f5cbe9bcac13708c35ace94846c6aaa02db2eb
Iury Roque <iury.roque@hotmail.com>
2022-08-07 16:40:28 -0300

    Feat:

--- app/backend/src/database/migrations/20220803195842-create-matchers.js
+++ app/backend/src/database/migrations/20220803195842-create-matchers.js
@@ -39,9 +39,27 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      team_name: {
+      home_team: {
         allowNull: false,
-        type: Sequelize.STRING(30),
+        type: Sequelize.INTEGER,
+        references: { model: 'teams', key: 'id' },
+      },
+      home_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      away_team: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'teams', key: 'id' },
+      },
+      away_team_goals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      in_progress: {
+        allowNull: false,
+        type: Sequelize.STRING(5),
       },
     });
   },
--- app/backend/src/database/migrations/20220803195852-create-users.js
+++ app/backend/src/database/migrations/20220803195852-create-users.js
@@ -21,7 +21,7 @@ module.exports = {
       },
       password: {
         allowNull: false,
-        type: Sequelize.STRING(50),
+        type: Sequelize.STRING(),
       },
     });
   },
@@ -47,7 +47,7 @@ module.exports = {
       },
       password: {
         allowNull: false,
-        type: Sequelize.STRING(50),
+        type: Sequelize.STRING(),
       },
     });
   },
